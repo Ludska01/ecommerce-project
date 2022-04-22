c693acdd0b916e7c89db06b4b84e0989932f1e3d
Luka Lazarević <luka.lazarevic@pmf.edu.rs>
2022-04-22 19:14:46 +0200

    petak 22

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -15,23 +15,29 @@
 @foreach ( $categories as $category )
   
 
-                <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa {{ $category->category_icon }}" aria-hidden="true"></i>{{ (session()->get('language')=='english') ? $category->category_name_en : $category->category_name_srb }}</a>
+                <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon fa {{ $category->category_icon }}" aria-hidden="true"></i>{{ (session()->get('language')=='serbian') ? $category->category_name_srb : $category->category_name_en  }}</a>
                   <ul class="dropdown-menu mega-menu">
                     <li class="yamm-content">
                       <div class="row">
+                        @php
+                              $subcats = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subcategory_name_en','ASC')->get();
+                            @endphp
+                            @foreach ($subcats as $subcat)
                         <div class="col-sm-12 col-md-3">
-                          <ul class="links list-unstyled">
-                            <li><a href="#">Dresses</a></li>
-                            <li><a href="#">Shoes </a></li>
-                            <li><a href="#">Jackets</a></li>
-                            <li><a href="#">Sunglasses</a></li>
-                            <li><a href="#">Sport Wear</a></li>
-                            <li><a href="#">Blazers</a></li>
-                            <li><a href="#">Shirts</a></li>
-                            <li><a href="#">Shorts</a></li>
+                          @php
+                          $subsubcats = App\Models\SubSubCategory::where('subcategory_id',$subcat->id)->orderBy('subsubcategory_name_en','ASC')->get();
+                        @endphp
+                         
+                          <h2 class="title">{{(session()->get('language')=='english') ? $subcat->subcategory_name_en : $subcat->subcategory_name_srb}}</h2>
+                          <ul class="links">
+                            @foreach ( $subsubcats as $subsubcat)
+                              <li><a href="#">{{(session()->get('language')=='english') ? $subsubcat->subsubcategory_name_en : $subsubcat->subsubcategory_name_srb}}</a></li>
+                            @endforeach
+                            
+                            
                           </ul>
                         </div>
-                       
+                       @endforeach
                       </div>
                       <!-- /.row --> 
                     </li>
